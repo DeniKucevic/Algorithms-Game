9ef297f3dac583a76ef79207557ba8598dbaa1af
Denis Kucevic <denikucevic@gmail.com>
2020-09-17 10:51:09 +0200

    fix prop validation in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,70 +9,70 @@ import StyledLabel from '../components/HomeComponents/styles/StyledLabel';
 import StyledTitle from '../components/HomeComponents/styles/StyledTitle';
 
 const Home = ({ config }) => {
-	const [stage, setStage] = useState({
-		size: [10, 10],
-		start: [0, 4],
-		finish: [9, 4],
-		checked: true,
-	});
-	const history = useHistory();
+  const [stage, setStage] = useState({
+    size: [10, 10],
+    start: [0, 4],
+    finish: [9, 4],
+    checked: true,
+  });
+  const history = useHistory();
 
-	const handleXSize = (x) => {
-		stage.size[1] = Number(x);
-	};
-	const handleYSize = (y) => {
-		stage.size[0] = Number(y);
-	};
-	const handleXStart = (x) => {
-		stage.start[1] = Number(x);
-	};
-	const handleYStart = (y) => {
-		stage.start[0] = Number(y);
-	};
-	const handleXFinish = (x) => {
-		stage.finish[1] = Number(x);
-	};
-	const handleYFinish = (y) => {
-		stage.finish[0] = Number(y);
-	};
-	const handleClick = () => {
-		config(stage);
-		history.push('/game');
-	};
+  const handleXSize = (x) => {
+    stage.size[1] = Number(x);
+  };
+  const handleYSize = (y) => {
+    stage.size[0] = Number(y);
+  };
+  const handleXStart = (x) => {
+    stage.start[1] = Number(x);
+  };
+  const handleYStart = (y) => {
+    stage.start[0] = Number(y);
+  };
+  const handleXFinish = (x) => {
+    stage.finish[1] = Number(x);
+  };
+  const handleYFinish = (y) => {
+    stage.finish[0] = Number(y);
+  };
+  const handleClick = () => {
+    config(stage);
+    history.push('/game');
+  };
 
-	return (
-		<StyledWrapper>
-			<StyledTitle>~ Pathfinding Game! ~</StyledTitle>
-			<StyledLabel>
-				Stage Size :
+  return (
+    <StyledWrapper>
+      <StyledTitle>~ Pathfinding Game! ~</StyledTitle>
+      <StyledLabel>
+        Stage Size :
 				<StyledInput placeholder="X" onChange={e => handleXSize(e.target.value)} />
-				<StyledInput placeholder="Y" onChange={e => handleYSize(e.target.value)} />
-			</StyledLabel>
-			<StyledLabel>
-				Starting Position :
+        <StyledInput placeholder="Y" onChange={e => handleYSize(e.target.value)} />
+      </StyledLabel>
+      <StyledLabel>
+        Starting Position :
 				<StyledInput placeholder="X" onChange={e => handleXStart(e.target.value)} />
-				<StyledInput placeholder="Y" onChange={e => handleYStart(e.target.value)} />
-			</StyledLabel>
-			<StyledLabel>
-				Finish Position :
+        <StyledInput placeholder="Y" onChange={e => handleYStart(e.target.value)} />
+      </StyledLabel>
+      <StyledLabel>
+        Finish Position :
 				<StyledInput placeholder="X" onChange={e => handleXFinish(e.target.value)} />
-				<StyledInput placeholder="Y" onChange={e => handleYFinish(e.target.value)} />
-			</StyledLabel>
-			<StyledLabel>
-				Auto Run?
+        <StyledInput placeholder="Y" onChange={e => handleYFinish(e.target.value)} />
+      </StyledLabel>
+      <StyledLabel>
+        Auto Run?
 				<StyledCheckBox checked={stage.checked} onChange={() => setStage({ ...stage, checked: !stage.checked })} />
-			</StyledLabel>
-			<StyledButton onClick={() => handleClick()}>Start Game</StyledButton>
-		</StyledWrapper>
-	);
+      </StyledLabel>
+      <StyledButton onClick={() => handleClick()}>Start Game</StyledButton>
+    </StyledWrapper>
+  );
 };
 
 export default Home;
 
 Home.defaultProps = {
-	config: {},
+  config: [],
 };
 
 Home.propTypes = {
-	config: PropTypes.objectOf(Array),
+  config: PropTypes.func,
 };
